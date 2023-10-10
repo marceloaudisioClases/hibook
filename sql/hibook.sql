b9862fa378acb2c0ca5bb91d6661eee7608de9fd
Hilet Aulas <hilet@HILET.LOCAL>
2023-10-10 09:12:21 -0300

    first commit

---
+++ sql/hibook.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-10-2023 a las 14:09:29
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hibook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` bigint(20) UNSIGNED NOT NULL,
+  `rate_id` bigint(20) UNSIGNED NOT NULL,
+  `text` text NOT NULL,
+  `state` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `profile_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `text` text NOT NULL,
+  `state` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rates`
+--
+
+CREATE TABLE `rates` (
+  `rate_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `stat_id` int(11) NOT NULL,
+  `rate` tinyint(4) NOT NULL,
+  `rater_id` int(10) UNSIGNED NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `state` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `rol_id` tinyint(4) NOT NULL,
+  `name` text NOT NULL,
+  `is_default` tinyint(4) NOT NULL DEFAULT 0,
+  `state` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `stats`
+--
+
+CREATE TABLE `stats` (
+  `stat_id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `color` text NOT NULL,
+  `icon` text NOT NULL,
+  `description` text NOT NULL,
+  `state` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `user` text NOT NULL,
+  `name` text NOT NULL,
+  `surname` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `rol_id` tinyint(4) NOT NULL,
+  `state` tinyint(4) NOT NULL DEFAULT 1,
+  `avatar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`profile_id`);
+
+--
+-- Indices de la tabla `rates`
+--
+ALTER TABLE `rates`
+  ADD PRIMARY KEY (`rate_id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`rol_id`);
+
+--
+-- Indices de la tabla `stats`
+--
+ALTER TABLE `stats`
+  ADD PRIMARY KEY (`stat_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `profiles`
+--
+ALTER TABLE `profiles`
+  MODIFY `profile_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rates`
+--
+ALTER TABLE `rates`
+  MODIFY `rate_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `rol_id` tinyint(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `stats`
+--
+ALTER TABLE `stats`
+  MODIFY `stat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
