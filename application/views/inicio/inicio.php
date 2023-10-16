3da1c4309c30821af2cb005affc260600a09646f
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-10-15 23:53:29 -0300

    Creación vista de inicio

---
+++ application/views/inicio/inicio.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    
+    <!-- Tabler Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css">
+
+    <title>Inicio</title>
+</head>
+<body>
+
+    <div class="page">
+        <header class="navbar navbar-expand-sm navbar-light d-print-none">
+            <div class="container-xl">
+                <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+                    <a href="#">
+                    <img src="https://preview.tabler.io/static/logo.svg" width="110" height="32" alt="Tabler" class="navbar-brand-image" />
+                    </a>
+                </h1>
+
+                <div class="navbar-nav flex-row order-md-last">
+                    <div class="nav-item">
+                    <a href="#" class="nav-link d-flex lh-1 text-reset p-0">
+                        <span class="avatar avatar-sm" style="background-image: url(/samples/avatars/002m.jpg)"></span>
+                        <div class="d-none d-xl-block ps-2">
+                        <div>Paweł Kuna</div>
+                        <div class="mt-1 small text-secondary">UI Designer</div>
+                        </div>
+                    </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="page-wrapper">
+            <div class="page-body">
+                <div class="container-xl">
+                    <div class="row row-deck row-cards">
+                    <div class="col-4">
+                        <div class="card">
+                        <div class="card-body" style="height: 10rem"></div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                        <div class="card-body" style="height: 10rem"></div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                        <div class="card-body" style="height: 10rem"></div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="card">
+                        <div class="card-body" style="height: 10rem"></div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <!-- Tabler Script -->
+    <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
+</body>
+</html>
