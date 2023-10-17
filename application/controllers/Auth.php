64c07303d48b63b7113fc2e9581320702bbf9439
Franco Spatocco <133174623+DeLaFuenteEzequiel@users.noreply.github.com>
2023-10-17 10:11:12 -0300

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -12,11 +12,17 @@ class Auth extends CI_Controller {
         $this->load->library('form_validation');
         $this->form_validation->set_rules("usuario", "Usuario", "required|trim|strtolower"); //name del form, Como se muestra a los humanos, condiciones separadas por |
         $this->form_validation->set_rules("password", "Contraseña", "required");
-        if ($this->form_validation->run() === false){
-            
+        if ($this->form_validation->run() === false){            
             $this->load->view("login");
         }
-        
+        else {
+            $this->load->model('user_model');
+            $usuario= set_value('usuario');
+            $password= set_value('password');
+            if ($u = $this->user_model->check_login($usuario, $password)){
+                
+            }
+        }
     }
 
     public function salir(){
