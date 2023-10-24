2dc44c438e200dd954232c08010fdb4b09ea4309
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-10-23 21:23:01 -0300

    No se que paso aca

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,7 +1,31 @@
-<php
+<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class auth extends CI_Controller
 
+class Auth extends CI_Controller {
 
-?>
+	public function index()
+	{
+		redirect("auth/ingresar");
+	}
 
+    public function ingresar(){
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules("usuario", "Usuario", "required|trim|strtolower"); //name del form, Como se muestra a los humanos, condiciones separadas por |
+        $this->form_validation->set_rules("password", "Contraseña", "required");
+        if ($this->form_validation->run() === false){            
+            $this->load->view("login");
+        }
+        else {
+            $this->load->model('user_model');
+            $usuario= set_value('usuario');
+            $password= set_value('password');
+            if ($u = $this->user_model->check_login($usuario, $password)){
+
+            }
+        }
+    }
+
+    public function salir(){
+
+    }
+}
