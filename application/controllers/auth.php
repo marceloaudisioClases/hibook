51da9766b1f56dff70ea59a8b06351f1c5b4ccd3
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-10-23 22:42:42 -0300

    Vista login funcional con plantilla

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();
+		$this->output->set_template('default');
+	}
+
 	public function index()
 	{
 		redirect("auth/ingresar");
@@ -13,7 +19,7 @@ class Auth extends CI_Controller {
         $this->form_validation->set_rules("usuario", "Usuario", "required|trim|strtolower"); //name del form, Como se muestra a los humanos, condiciones separadas por |
         $this->form_validation->set_rules("password", "Contraseña", "required");
         if ($this->form_validation->run() === false){            
-            $this->load->view("login");
+            $this->load->view("user_auth");
         }
         else {
             $this->load->model('user_model');
