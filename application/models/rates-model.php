e5316d4383a9b7b4a65c489315f4b56d268a4b70
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-10-23 22:57:47 -0300

    Agregado rates model + fix inner joins

--- application/models/rates-model.php
+++ application/models/rates-model.php
@@ -1,49 +1,47 @@
 <?php
 
-class usuarios_model extends CI_Model{
+class rates_model extends CI_Model{
 
-    protected $table= "users";
-    protected $pk= "user_id";
+    protected $table= "rates";
+    protected $pk= "rate_id";
 
     public function default_select(){
         $this->db->select($this->table.".*");
     }
 
     public function default_join(){
-        $this->db->join("roles", $this->table.".rol_id= roles.rol_id", "inner");
-        $this->db->join("profiles", $this->table.".".$this->pk"= profile.user_id", "left");
+        $this->db->join("comments", $this->table.".".$this->pk."= comments.rate_id", "left");
     }
 
-    public function create_user($data) {
+    public function create_rate($data) {
         $this->db->insert($this->table, $data);
         return $this->db->insert_id();
     }
 
-    public function get_all_users() {
+    public function get_all_rates() {
         $this->default_select();
         $this->default_join();
         return $this->db->get($this->table)->result_array();
     }
     
-    public function get_user_by_id($user_id) {
+    public function get_rate_by_id($rate_id) {
         $this->default_select();
         $this->default_join();
-        $this->db->where($this->pk, $user_id);
+        $this->db->where($this->pk, $rate_id);
         return $this->db->get($this->table)->row_array();
     }
     
-    public function update_user($user_id, $data) {
-        $this->db->where($this->pk, $user_id);
+    public function update_rate($rate_id, $data) {
+        $this->db->where($this->pk, $rate_id);
         $this->db->update($this->table, $data);
         return $this->db->affected_rows();
     }
     
-    public function delete_user($user_id) {
-        $this->db->where($this->pk, $user_id);
+    public function delete_rate($rate_id) {
+        $this->db->where($this->pk, $rate_id);
         $this->db->delete($this->table);
         return $this->db->affected_rows();
     }
-    
 
 }
 
