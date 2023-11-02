0d0dc8b3e6b707a07cc2a7a60b279fe8b49e057a
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-11-02 16:35:25 -0300

    Quitados etiquetas innecesarias

--- application/views/usuario/usuario.php
+++ application/views/usuario/usuario.php
@@ -1,78 +1,57 @@
-<!DOCTYPE html>
-<html lang="es">
+ <div class="page">
+     <div class="page-wrapper">
+         <div class="page-body">
+             <div class="container-xl">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                 <!-- Perfil de usuario buscado -->
+                 <div class="row row-deck row-cards">
+                     <div class="col-10 offset-1">
+                         <div class="card">
+                             <div class="card-body" style="height: 10rem">
+                                 <h1>Perfil de "Nombre de Usuario"</h1>
+                                 <div class="row g-4 py-4">
+                                     <div class="col-2 offset-1">
+                                         <p><b>Fellowship</b>: 3</p>
+                                     </div>
+                                     <div class="col-2">
+                                         <p>Critical-thinking: 4</p>
+                                     </div>
+                                     <div class="col-2">
+                                         <p><b>Leadership</b>: 4</p>
+                                     </div>
+                                     <div class="col-2">
+                                         <p>Teamwork: 4</p>
+                                     </div>
+                                     <div class="col-2">
+                                         <p><b>Teaching</b>: 4</p>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
 
-    <!-- Tabler Styles -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css">
+                 <div class="row row-deck row-cards py-2">
+                     <!-- Form enviar evaluacion -->
+                     <div class="col-5 offset-1">
+                         <div class="card">
+                             <div class="card-body" style="height: 10rem">
+                                 <p class="card-title">Envía una Evaluación:</p>
 
-    <title>HiBook</title>
-</head>
+                             </div>
+                         </div>
+                     </div>
+                     <!-- Novedades usuario buscado -->
+                     <div class="col-5">
+                         <div class="card">
+                             <div class="card-body" style="height: 10rem">
+                                 <p class="card-title">Novedades de "Nombre de Usuario"</p>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
 
-<body>
-    <div class="page">
-        <div class="page-wrapper">
-            <div class="page-body">
-                <div class="container-xl">
-
-                    <!-- Perfil de usuario buscado -->
-                    <div class="row row-deck row-cards">
-                        <div class="col-10 offset-1">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem">
-                                    <h1>Perfil de "Nombre de Usuario"</h1>
-                                    <div class="row g-4 py-4">
-                                        <div class="col-2 offset-1">
-                                            <p><b>Fellowship</b>: 3</p>
-                                        </div>
-                                        <div class="col-2">
-                                            <p>Critical-thinking: 4</p>
-                                        </div>
-                                        <div class="col-2">
-                                            <p><b>Leadership</b>: 4</p>
-                                        </div>
-                                        <div class="col-2">
-                                            <p>Teamwork: 4</p>
-                                        </div>
-                                        <div class="col-2">
-                                            <p><b>Teaching</b>: 4</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row row-deck row-cards py-2">
-                        <!-- Form enviar evaluacion -->
-                        <div class="col-5 offset-1">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem">
-                                    <p class="card-title">Envía una Evaluación:</p>
-
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Novedades usuario buscado -->
-                        <div class="col-5">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem">
-                                    <p class="card-title">Novedades de "Nombre de Usuario"</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <!-- Tabler Script -->
-    <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
-</body>
-
-</html>
+             </div>
+         </div>
+     </div>
+ </div>
