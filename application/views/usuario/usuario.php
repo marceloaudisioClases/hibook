93c57c5c06df5e33e4561783f4f511387705cf16
Patricio002 <patricio_labarden@hotmail.com>
2023-11-02 22:22:19 -0300

    Slider Range - Novedades
    Slider Range visualmente terminado, y lista de Novedades visualmente terminado. Falta funcionalidad interna

--- application/views/usuario/usuario.php
+++ application/views/usuario/usuario.php
@@ -30,12 +30,21 @@
                         </div>
                     </div>
                 </div>
+                
                 <div class="row row-deck row-cards py-2">
                     <!-- Form enviar evaluacion -->
                     <div class="col-5 offset-1">
                         <div class="card">
-                            <div class="card-body" style="height: 10rem">
+                            <div class="card-body" style="height: 20rem">
                                 <p class="card-title">Envía una Evaluación:</p>
+                                Amabilidad
+                                <div><input type="range" min="0" max="5" step="1" value="0"/></div>
+                                Compañerismo
+                                <div><input type="range" min="0" max="5" step="1" value="0"/></div>
+                                Pensamiento Critico
+                                <div><input type="range" min="0" max="5" step="1" value="0"/></div>
+                               
+                                <br><br><div align="center"><a href="#" class="btn btn-outline-info" id="btnConfirmarValoracion">Confirmar Valoración</a></div>
                             </div>
                         </div>
                     </div>
@@ -44,6 +53,14 @@
                         <div class="card">
                             <div class="card-body" style="height: 10rem">
                                 <p class="card-title">Novedades de: <?php echo $usuario["nombre"]; ?></p>
+                                <ul class="list-group list-group-flush" id="listaNovedades">
+                                <li class="list-group-item" id="novedad">Novedad1</li>
+                                <li class="list-group-item">Novedad2</li>
+                                <li class="list-group-item">Novedad3</li>
+                                <li class="list-group-item">Novedad4</li>
+                                <li class="list-group-item">Novedad5</li>
+                                </ul>
+
                             </div>
                         </div>
                     </div>
